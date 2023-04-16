dc546e10be46cf884fa7f164ca22df7a24eeac4a
Anisi <Anisi@reddiPC>
2023-04-16 11:29:39 +0300

    S0M4T4 done.

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+    	int maxFromTwo;
+    	maxFromTwo = first >= second ?
+    			first:
+    			second;
+    	int max = maxFromTwo >= third ?
+    			maxFromTwo:
+    			third;
+    	System.out.println(max);
     }
 }
